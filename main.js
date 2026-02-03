69fc301b9e4037e9d8734f7d38c5ec95550cb864
YasirAfridi-FSDeveloper <afridiyasir921@gmail.com>
2026-02-03 16:22:53 +0500

    Add to cart page and  added item shown in cart page

--- main.js
+++ main.js
@@ -1,47 +1,59 @@
+// Load cart from localStorage
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
 // Select elements
-let addBtn = document.getElementById("addToCart");
-let removeBtn = document.getElementById("removeFromCart");
 let cartCount = document.getElementById("cartCount");
+let addBtns = document.querySelectorAll(".add-to-cart");
+let removeBtns = document.querySelectorAll(".remove-from-cart");
 
-// Cart array
-let cart = [];
-
-// Add to cart
-addBtn.addEventListener("click", function () {
-
-  let product = {
-    name: "Skynish Skin Brightening Cream",
-    price: 2000
-  };
-
-  cart.push(product);
-
-  updateCart();
-
-//   alert("Product added to cart!");
-});
-
-
-// Remove from cart
-removeBtn.addEventListener("click", function () {
-
-  if (cart.length > 0) {
-
-    cart.pop(); // Remove last item
-    updateCart();
-
-    // alert("Product removed!");
-
-  } else {
-
-    // alert("Cart is empty!");
-  }
-
-});
-
-
-// Update cart number
-function updateCart() {
-  cartCount.innerText = cart.length;
-  console.log("Cart:", cart);
+// Update cart count
+function updateCartCount() {
+  if (cartCount) cartCount.innerText = cart.length;
 }
+
+// Save cart
+function saveCart() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
+}
+
+// ADD BUTTON
+addBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    let name = btn.dataset.name;
+    let price = btn.dataset.price;
+
+    cart.push({name, price});
+    saveCart();
+  });
+});
+
+// REMOVE BUTTON
+removeBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    let name = btn.dataset.name;
+    let index = cart.findIndex(item => item.name === name);
+    if (index !== -1) {
+      cart.splice(index, 1);
+      saveCart();
+    }
+  });
+});
+
+// SHOW CART PAGE
+let cartList = document.getElementById("cartList");
+let total = document.getElementById("total");
+
+if (cartList) {
+  let sum = 0;
+  cart.forEach(item => {
+    let li = document.createElement("li");
+    li.innerText = `${item.name} - Rs ${item.price}`;
+    cartList.appendChild(li);
+    sum += Number(item.price);
+  });
+  total.innerText = "Total: Rs " + sum;
+}
+
+// Initialize count on page load
+updateCartCount();
